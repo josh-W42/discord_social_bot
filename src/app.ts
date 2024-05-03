1f5538c9e15444049a78e0a8f4604d21831a5266
Josh W <wilson.jd022@gmail.com>
2024-05-02 22:33:36 -0700

    Added basic test route to confirm discord api key was properly created

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
+import "dotenv/config";
 import express from "express";
+import { DiscordRequest } from "./utils";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get("/api/test", async (req: express.Request, res: express.Response) => {
+  try {
+    const discordResponse = await DiscordRequest("/users/@me/guilds", {});
+
+    const data = await discordResponse.json();
+
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Listening on PORT: ", PORT);
