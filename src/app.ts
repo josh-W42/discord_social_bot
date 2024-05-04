95af1ab9f1c5778249e9efae75ecdffe5f010231
Josh W <wilson.jd022@gmail.com>
2024-05-04 07:04:40 -0700

    Updated future comment notes

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { GoogleService } from "./google";
 import fs from "fs";
 
 /**
- * What needs to be saved (IF THIS APPLIES TO MORE THAN ONE SERVER):
+ * What needs to be saved (IF THIS APPLIES TO MORE THAN ONE SERVER / GUILD):
  * 1. The Youtube Channel that someone wants tracked.
  * 2. The id of a Guild associated with this tracking.
  * 3. The id of a channel that the bot will post the link to.
